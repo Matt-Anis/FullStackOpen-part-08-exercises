0be3f1b2f58094c484d4b28cd40cc52429a73462
Matt Anis <webdev.omis@gmail.com>
2026-07-02 00:06:17 +0100

    refactored to use the cursom hook for book subscription

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -6,29 +6,11 @@ import LoginForm from "./components/LoginForm";
 import Recommended from "./components/Recommended";
 import { useAuth } from "./hooks/useAuth";
 import { useBookSubscription } from "./hooks/subscriptions/useBookSubscription";
-import { useSubscription, useApolloClient } from "@apollo/client/react";
-import { BOOK_ADDED } from "./queries";
-import { addBookToCache, addAuthorToCache } from "./utils/apolloCache";
 
 const App = () => {
   const { token, logout } = useAuth();
   const [page, setPage] = useState("authors");
-  const client = useApolloClient();
-  useSubscription(BOOK_ADDED, {
-    onData: ({ data }) => {
-      console.log("new book recieved!");
-      const addedBook = data.data.bookAdded;
-      window.alert(`${addedBook.title} added`);
-      addBookToCache(client.cache, addedBook);
-      addAuthorToCache(client.cache, addedBook?.author);
-    },
-    onError: (error) => {
-      console.log("subscription error:", error);
-    },
-    onComplete: () => {
-      console.log("subscription completed/closed");
-    },
-  });
+  useBookSubscription();
 
   return (
     <div>
